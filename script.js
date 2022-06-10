f3ffc20090025a98db840398ab18c4b7bc136737
Rianfiore <rianfiore@gmail.com>
2022-06-10 11:05:14 -0300

    updating challenge

---
+++ script.js
@@ -0,0 +1,12 @@
+const name = "Rian",
+  age = 22,
+  actualYear = 2022,
+  quartaFeira = false;
+
+console.log("Meu nome é:", name, ", nasci em: " + (actualYear - age) + ".");
+
+if (quartaFeira) {
+  console.log("Hoje é quarta-feira!");
+} else {
+  console.log("Hoje não é quarta-feira!");
+}
